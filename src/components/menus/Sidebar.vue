d5bb929e6a64e0608c48148c939a81587e053d50
ArthurSBs <luminite.snt@gmail.com>
2025-09-03 11:41:47 -0300

    Dashboard Stuff

--- src/components/menus/Sidebar.vue
+++ src/components/menus/Sidebar.vue
@@ -1,23 +1,36 @@
 <template>
-    <div class="sidebar-menu" :class="{collapsed}">
-        <button class="toggle-sidebar" @click="toggleSidebar">YAY</button>
-        <nav class="sidebar"></nav>
+    <div :class="['sidebar-menu', { collapsed: isCollapsed }]">
+        <div class="collapse-container">
+            <button class="toggle-sidebar link" @click="toggleSidebar">{{ isCollapsed ? '->' : '<-' }}</button> 
+        </div>
+        <nav v-if="!isCollapsed" class="sidebar"><p></p></nav>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const isCollapsed = ref(false)
+
+const toggleSidebar = () => {
+    isCollapsed.value = !isCollapsed.value
+}
+
 </script>
 
 <style lang="scss">
-.sidebar-menu{
-    position: fixed;
 
-    width: 8dvw;
+.sidebar-menu{
+    position: relative;
+
+    width: 10dvw;
     height: 100dvh;
 
-    display: flex;
-    justify-content: right;
-    align-items: center;
+    transition: width 0.5s ease, transform 0.3s ease;
+    &.collapsed{
+        transition: width 0.5s ease, transform 0.3s ease;
+        width: 0dvw;
+    }
 
     background-color: var(--main-color);
 }
@@ -28,10 +41,32 @@
     align-items: center;
 }
 
+.collapse-container{
+    height: 100dvh;
+
+    padding-left: 40px;
+
+    display: flex;
+    justify-content: right;
+    align-items: center;
+}
+
 .toggle-sidebar{
     width: 50px;
     height: 50px;
 
-    margin-left: 75px;
+    color: var(--text-color);
+    font-size: 30px;
+    background-color: transparent;
+    border: none
+}
+
+.test{
+    position: relative;
+
+    width: 30dvw;
+    height: 20dvh;
+
+    background-color: red;
 }
 </style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -4,8 +4,47 @@ import Sidebar from '@/components/menus/Sidebar.vue';
 </script>
 
 <template>
-    <Sidebar/>
+    <div class="layout">
+        <Sidebar/>
+        <div class="sections-container">
+            <div class="section-top">
+                <div class="merda"></div>
+            </div>
+            <div class="section-top">
+                <div class="merda"></div>
+            </div>
+        </div>
+        
+    </div>
 </template>
 
 <style lang="scss">
+.layout{
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+
+    gap: 15px;
+}
+
+.sections-container{
+    display: flex;
+    flex-direction: column; // stack sections vertically
+    gap: 15px;
+}
+
+.section-top{
+    width: 100%;
+    border: solid 1px green;
+
+    display: flex;
+}
+
+.merda{
+    height: 30dvh;
+    width: 20dvw;
+
+    background-color: red;
+}
+
 </style>
